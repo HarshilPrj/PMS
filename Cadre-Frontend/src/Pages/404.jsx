745867c6a41365d8946a04027f1f2aa07ab06f1c
Harshil <prajapatiharshil625@gmail.com>
2024-02-06 14:08:12 +0530

    .

--- Cadre-Frontend/src/Pages/404.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NotFound() {
-    return (
-        <div className="notfound">
-            <h1 style={{ fontSize: "100px" }}>404</h1>
-        </div>
-    );
-}
